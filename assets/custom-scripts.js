3f6cf382f0badc3b3ff3a8fb4aa5291148bff871
kwanglo-huinno <kwanglo@huinno.com>
2022-12-09 17:49:01 +0900

    Add data and asset folder
    - Add sample data
    - Add empty JS asset

---
+++ assets/custom-scripts.js
@@ -0,0 +1,9 @@
+var config = {
+    modeBarButtonsToAdd: [
+        {
+            name: 'button1',
+            icon: Plotly.Icons.pencil,
+            direction: 'up',
+            click: function(gd) {alert('button1')}
+        }]
+}
